8b9a6e49ce26a7e3a85702bf0529d0564e8ceecd
merchantry <fivanovic98@gmail.com>
2025-07-26 13:51:30 +0200

    removed blob 2

--- src/components/Background/Background.js
+++ src/components/Background/Background.js
@@ -1,12 +1,10 @@
 import './Background.css';
 import { ReactComponent as Blob1 } from '../../img/blob_1.svg';
-import { ReactComponent as Blob2 } from '../../img/blob_2.svg';
 
 function Background() {
   return (
     <div className="Background">
       <Blob1 className="Blob1 Blob" />
-      <Blob2 className="Blob2 Blob" />
     </div>
   );
 }
